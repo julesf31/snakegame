9baca8a750235fe9b35e2646a7a2acc9d2056440
julesf31 <78370331+julesf31@users.noreply.github.com>
2021-04-05 11:28:42 -0500

    got collision working

--- snakegame.py
+++ snakegame.py
@@ -25,9 +25,16 @@ xpos = 1
 board[ypos][xpos] = "x"
 
 def new_fruit_pos(board, lenth):
-  if board[ypos][xpos] == '0':
+  if ypos == 0:
+    return 
+  elif ypos == 9:
+    return
+  elif board[ypos][xpos] == '0':
     fruitypos = random.randint(1, 7)
     fruitxpos = random.randint(1, 7)
+    if board[fruitypos][fruitxpos] == 'x':
+      fruitypos = random.randint(1, 7)
+      fruitxpos = random.randint(1, 7)
     board[fruitypos][fruitxpos] = '0'
     lenth += 1
   return lenth
@@ -40,32 +47,61 @@ fruitypos = random.randint(1, 7)
 fruitxpos = random.randint(1, 7)
 board[fruitypos][fruitxpos] = '0'
 
+def check_for_collision(ypos, xpos, board):
+  global l
+  if ypos == 0:
+    l = 1
+    return l
+  elif ypos == 9:
+    l += 1
+    return l
+  elif board[ypos][xpos] == 'x':
+    l += 1
+    return l
+  elif board[ypos][xpos] == '|':
+    l += 1
+    return l
+
 print_board(board)
 lenth = 1
-win = 0
-while win != 1:
+l = 0
+while 1:
   move = input('Enter direction (wasd): ')
   if move == 'w':
     board[ypos][xpos] = ' '
     ypos -= 1
+    check_for_collision(ypos, xpos, board)
+    if l == 1:
+      break
     new_fruit_pos(board, lenth)
     board[ypos][xpos] = 'x'
     print_board(board)
   elif move == 's':
     board[ypos][xpos] = ' '
     ypos += 1
+    check_for_collision(ypos, xpos, board)
+    if l == 1:
+      break
     new_fruit_pos(board, lenth)
     board[ypos][xpos] = 'x'
     print_board(board)
   elif move == 'a':
     board[ypos][xpos] = ' '
     xpos -= 1
+    check_for_collision(ypos, xpos, board)
+    if l == 1:
+      break
     new_fruit_pos(board, lenth)
     board[ypos][xpos] = 'x'
     print_board(board)
   elif move == 'd':
     board[ypos][xpos] = ' '
     xpos += 1
+    check_for_collision(ypos, xpos, board)
+    if l == 1:
+      break
     new_fruit_pos(board, lenth)
     board[ypos][xpos] = 'x'
     print_board(board)
+
+print('bad')
